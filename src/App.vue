f15189a2f28e6f2e398e512f9af1070433e64963
Patrick Kennedy <pakattack161@gmail.com>
2021-03-10 00:48:03 -0800

    header: basic header, not much to add

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<template lang="pug">
+#app
+  the-header
+  router-view.container.max-width-md
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import TheHeader from '#/TheHeader.vue'
+
+export default {
+  name: 'app',
+  components: { TheHeader },
 }
+</script>
 
-#nav {
-  padding: 30px;
+<style lang="sass">
+html, body
+  height: 100%
+  background-color: var(--color-contrast-low)
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: var(--color-contrast-high)
+  height: 100%
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+header.header
+  .container.max-width-lg
+    router-link(to="/").branding Chicago Horrors
+    
+  
+</template>
+
+<script>
+export default {
+  name: 'the-header',
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {}
+  },
+
+  created() {},
+  mounted() {},
+  watch: {},
+  computed: {},
+  methods: {},
+}
+</script>
+
+<style lang="sass">
+\:root
+  --header-height: 3rem
+</style>
+
+<style lang="sass" scoped>
+.header
+  height: var(--header-height)
+  width: 100%
+  background-color: var(--color-white)
+
+  .container
+    height: 100%
+    display: flex
+    justify-content: space-between
+    align-items: center
+</style>

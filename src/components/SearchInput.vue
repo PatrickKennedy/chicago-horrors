0672a1d304b3189a5f5be9c05383e514fd9b4800
Patrick Kennedy <pakattack161@gmail.com>
2021-03-10 23:07:49 -0800

    search-input: added a wrapper to emit a "cleared" event when cleared

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+input(
+  :value=value
+  @input="handleInput"
+  :attrs="$attrs"
+  :listeners="$listeners"
+)
+</template>
+
+<script>
+/** Input wrapper to add a "cleared" event when it's empty */
+export default {
+  name: 'search-input',
+  props: ['value'],
+  methods: {
+    handleInput($evt) {
+      const value = $evt.target.value
+      this.$emit('input', value)
+      if (!value) this.$emit('cleared')
+    },
+  },
+}
+</script>

29dc0cee71abc8cafef88208e8d2107c60ad66bc
Patrick Kennedy <pakattack161@gmail.com>
2021-03-10 23:08:18 -0800

    api: extend timeout for larger queries (e.g. "grease" in violations)

--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const createClient = () => {
   return axios.create({
     baseURL: 'https://data.cityofchicago.org/resource/cwig-ma7x.json',
-    timeout: 1000,
+    timeout: 2500,
     headers: {
       'X-App-Token': 'LCQ6YR03g5AhmvsOkYfPGeSiL',
     },
